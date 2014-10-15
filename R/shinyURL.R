6d73cdb5104248b3acd58319107134ca0e9fda95
aoles <andrzej.oles@gmail.com>
2014-10-15 17:47:31 +0200

    Initial working example

---
+++ R/shinyURL.R
@@ -0,0 +1,49 @@
+#' Encode current application state into URL
+#' @param session parameter passed from the shiny "server function"
+#' @param input parameter passed from the shiny "server function"
+#' @import shiny
+#' @export 
+encodeShinyURL = function(session, input) {
+  observe({
+  ## all.names = FALSE excludes objects with a leading dot, in this case the ".url" field to avoid self-dependency
+  inputValues = reactiveValuesToList(input, all.names = FALSE)
+  updateTextInput(session, ".url", value = paste0(
+    session$clientData$url_protocol, "//",
+    session$clientData$url_hostname,
+    ## add port number if present
+    if( (port=session$clientData$url_port)!="" ) paste0(":", port),
+    session$clientData$url_pathname,        
+    ## encode widget state
+    "?", paste(names(inputValues), inputValues, sep = "=", collapse = "&") 
+    ))
+  }, priority = -100, suspended = TRUE)
+}
+
+#' Initialize application state from URL
+#' @param session parameter passed from the shiny "server function"
+#' @param nestedDependency set to \code{TRUE} if you use reactive UI
+#' @param self self-reference
+#' @param encode reference to the URL encoder object
+#' @param resume objects (Observers) which start in a suspended state and need to be resumed after initialization finishes 
+#' @export
+initFromURL = function(session, nestedDependency = FALSE, self, encode, resume = encode) {
+  observe({
+    # execute only once at startup
+    self$suspend()
+      
+    query = parseQueryString(session$clientData$url_search)
+    
+    #initial pass needed due to nested dependency of concentration on drug 
+    if ( isTRUE(nestedDependency) ) .initInputs(session, query)
+    
+    session$onFlushed( function() {
+      .initInputs(session, query)
+      for(i in resume) i$resume()
+    })
+  })
+}
+
+.initInputs = function(session, query) {
+  for(i in seq_along(query))
+    session$sendInputMessage(names(query)[i], list( value = ifelse(is.na((l = as.logical((q=query[[i]])))), q, l)) )
+}
--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-shinyURL
-========
+## Installation
 
-Save and restore state od shiny apps by encoding it in the app's URL
+    # install.packages("devtools")
+    devtools::install_github("aoles/shinyURL")
+
+## Use
+
+1. Add to 'server.R' and 'ui.R'
+    
+        library('shinyURL')
+
+2. Add the textfield for the URL to 'ui.R'
+
+        textInput(".url", "", value = "")
+
+3. To 'server.R' add the following
+
+        init = initFromURL(session, self = init, encode = encode)
+        
+        encode = encodeShinyURL(session, input)
+        
+    Note that you need to provide a self reference and a reference to the 'encodeShinyURL' object in the call to 'initFromURL'.
