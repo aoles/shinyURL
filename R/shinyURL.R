568e11e78655e5aa8e9e072bcbd8619771461ad9
aoles <andrzej.oles@gmail.com>
2014-12-11 13:20:06 +0100

    Add support for range sliders

--- R/shinyURL.R
+++ R/shinyURL.R
@@ -6,7 +6,12 @@
 #' @import shiny
 #' @export 
 shinyURL = function(session, inputId=".url") {
-  .inputValues <<- reactive(reactiveValuesToList(session$input, all.names=FALSE))
+  .inputValues <<- reactive({
+    inputValues = reactiveValuesToList(session$input, all.names=FALSE)
+    ## remove ggvis specific inputs
+    idx = grep("_mouse_(over|out)$", names(inputValues))
+    if ( length(idx) > 0 ) inputValues = inputValues[-idx]
+    inputValues})
   .queryValues <<- isolate(parseQueryString(session$clientData$url_search, nested=TRUE))
   
   ## initialize from query string
@@ -31,37 +36,46 @@ encodeShinyURL = function(session) {
   ## all.names = FALSE excludes objects with a leading dot, in this case the ".url" field to avoid self-dependency
   inputValues = .inputValues()
   
-  ## discard individual elements of CheckboxGroups and expand CheckboxGroup vectors
-  names = names(inputValues)
-  i = 0
+  ## NOTE: the default values of checkbox groups are encoded as TRUE/FALSE 
+  ## values of individual elements named as checkboxGroupNameX, where X is the
+  ## element index; the _values_ set by user are stored as a vector under the
+  ## checkboxGroupName
+  
+  ## sort to catch checkboxGroupNames before individual element names  
+  names = sort(names(inputValues))
   addValues = NULL
-  while (i < length(names) ) {        
-    i = i+1
+  i = 1L
+  while (i <= length(names) ) {        
     n = names[i]
-    idx = grep(sprintf("^%s[0-9]+", n), names)
+    idx = grep(sprintf("^%s[0-9]+", n), names)    
     
-    ## encountered CheckboxGroup
-    if ( length(idx)>0 ) {
+    if ( length(idx) == 0L ) {
+      ## advance to the next input
+      i = i + 1L
+    } else {
+      ## encountered CheckboxGroup; do not increment i because the corresponding
+      ## element is removed from the list
       value = inputValues[[n]]
       names = names[-c(i, idx)]
-      if ( !is.null(value) ) {
-        values = as.list(value)
-        names(values) = sprintf("%s[%s]", n, seq_along(value))
-        addValues = c(addValues, values)
-      }
+      ## this is important to be able to have all checkboxes unchecked
+      if ( is.null(value) ) value = ""
+      values = as.list(value)
+      ## expand CheckboxGroup vectors
+      names(values) = sprintf("%s[%s]", n, seq_along(value))
+      addValues = c(values, addValues)
     }
   }
   inputValues = inputValues[names]
   inputValues = c(inputValues, addValues)
   
-  ## compress TRUE/FALSE to T/F
+  ## encode TRUE/FALSE as T/F
   inputValues = lapply(inputValues, function(x) {
     if ( is.logical(x) ) {
       if (isTRUE(x)) "T" else "F"
     } else x  
   })
   
-  updateTextInput(session, inputId, value = paste0(
+  url = paste0(
     session$clientData$url_protocol, "//",
     session$clientData$url_hostname,
     ## add port number if present
@@ -69,7 +83,9 @@ encodeShinyURL = function(session) {
     session$clientData$url_pathname,        
     ## encode widget state
     "?", paste(names(inputValues), inputValues, sep = "=", collapse = "&") 
-    ))
+  )
+  
+  updateTextInput(session, inputId, value = url)
   }, priority = -100)
 }
 
@@ -93,7 +109,7 @@ initFromURL = function(session, nestedDependency = FALSE, self, encode, resume =
       self$suspend()
       return(NULL)
     }
-    
+
     ## iterate through available inputs as long as there are any uninitialized values in .queryValues
     ## the expression below depends on inputs which is neccassary to restore dynamic UIs  
     updateValues = intersect(names(.inputValues()), names(.queryValues))
@@ -102,13 +118,28 @@ initFromURL = function(session, nestedDependency = FALSE, self, encode, resume =
 
     if ( length(idx) > 0 ) .queryValues <<- .queryValues[-idx]
    
-    .initInputs(session, updateValues)
+    ## schedule the update only after all input messages have been sent out (see
+    ## the 'flushOutput' function in shiny.R). This is to avoid potential
+    ## overwriting by some update events from user code
+    session$onFlushed(function() {
+      .initInputs(session, updateValues)
+    })
     
     invisible(NULL)
   })
 }
 
 .initInputs = function(session, query) {
-  for(i in seq_along(query))
-    session$sendInputMessage(names(query)[i], list( value = ifelse(is.na((l = as.logical((q=query[[i]])))), q, l)) )
+  for (i in seq_along(query)) {
+    q = query[[i]]
+    if (!is.list(q)) {
+      ## parse numeric vectors (needed by range sliders)
+      if ( isTRUE(grepl("^c\\( *([[:digit:]]+, *)*[[:digit:]]+ *\\)$", q)) )
+        q = eval(parse(text=q))
+      ## decode TRUE/FALSE
+      else if ( isTRUE(grepl("^[TF]$", q)) )
+        q = as.logical(q)
+    }
+    session$sendInputMessage(names(query)[i], list(value=q))
+  }
 }
