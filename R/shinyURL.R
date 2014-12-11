ff3d476c83add95c9c0ed0384127462dcf9844d5
aoles <andrzej.oles@gmail.com>
2014-12-11 13:20:06 +0100

    A couple of bug fixes

--- R/shinyURL.R
+++ R/shinyURL.R
@@ -28,27 +28,41 @@ encodeShinyURL = function(session) {
 
 .encodeURL = function(session, inputId) {
   observe({
+  #browser()
   ## all.names = FALSE excludes objects with a leading dot, in this case the ".url" field to avoid self-dependency
   inputValues = .inputValues()
   
-  ## discard individual elements of CheckboxGroups and expand CheckboxGroup vectors
-  names = names(inputValues)
-  i = 0
+  ## remove ggvis specific inputs
+  idx = grep("_mouse_(over|out)$", names(inputValues))
+  if ( length(idx) > 0 ) inputValues = inputValues[-idx]
+  
+  ## NOTE: the default values of checkbox groups are encoded as TRUE/FALSE 
+  ## values of individual elements named as checkboxGroupNameX, where X is the
+  ## element index; the _values_ set by user are stored as a vector under the
+  ## checkboxGroupName
+  
+  ## sort to catch checkboxGroupNames before individual element names  
+  names = sort(names(inputValues))
   addValues = NULL
-  while (i < length(names) ) {        
-    i = i+1
+  i = 1L
+  while (i <= length(names) ) {        
     n = names[i]
-    idx = grep(sprintf("^%s[0-9]+", n), names)
+    idx = grep(sprintf("^%s[0-9]+", n), names)    
     
-    ## encountered CheckboxGroup
-    if ( length(idx)>0 ) {
+    if ( length(idx) == 0L ) {
+      ## advance to the next input
+      i = i + 1L
+    } else {
+      ## encountered CheckboxGroup; do not increment i because the corresponding
+      ## element is removed from the list
       value = inputValues[[n]]
       names = names[-c(i, idx)]
-      if ( !is.null(value) ) {
-        values = as.list(value)
-        names(values) = sprintf("%s[%s]", n, seq_along(value))
-        addValues = c(addValues, values)
-      }
+      ## this is important to be able to have all checkboxes unchecked
+      if ( is.null(value) ) value = ""
+      values = as.list(value)
+      ## expand CheckboxGroup vectors
+      names(values) = sprintf("%s[%s]", n, seq_along(value))
+      addValues = c(values, addValues)
     }
   }
   inputValues = inputValues[names]
@@ -61,7 +75,7 @@ encodeShinyURL = function(session) {
     } else x  
   })
   
-  updateTextInput(session, inputId, value = paste0(
+  url = paste0(
     session$clientData$url_protocol, "//",
     session$clientData$url_hostname,
     ## add port number if present
@@ -69,7 +83,9 @@ encodeShinyURL = function(session) {
     session$clientData$url_pathname,        
     ## encode widget state
     "?", paste(names(inputValues), inputValues, sep = "=", collapse = "&") 
-    ))
+  )
+  
+  updateTextInput(session, inputId, value = url)
   }, priority = -100)
 }
 
@@ -88,12 +104,13 @@ initFromURL = function(session, nestedDependency = FALSE, self, encode, resume =
 
 .initFromURL = function(session, self) {
   observe({
+    #browser()
     ## suspend if nothing to do 
     if ( length(.queryValues) == 0L ) {
       self$suspend()
       return(NULL)
     }
-    
+
     ## iterate through available inputs as long as there are any uninitialized values in .queryValues
     ## the expression below depends on inputs which is neccassary to restore dynamic UIs  
     updateValues = intersect(names(.inputValues()), names(.queryValues))
@@ -102,7 +119,12 @@ initFromURL = function(session, nestedDependency = FALSE, self, encode, resume =
 
     if ( length(idx) > 0 ) .queryValues <<- .queryValues[-idx]
    
-    .initInputs(session, updateValues)
+    ## schedule the update only after all input messages have been sent out (see
+    ## the 'flushOutput' function in shiny.R). This is to avoid potential
+    ## overwriting by some update events from user code
+    session$onFlushed(function() {
+      .initInputs(session, updateValues)
+    })
     
     invisible(NULL)
   })
--- README.md
+++ README.md
@@ -22,6 +22,8 @@
 
 ## Caveats
 
+### Long URLs
+
 The state of a shiny app gets saved by encoding its input values into an URL. To keep the URL compact and to avoid problems caused by the URL length limit (around 2000 characters) there are some points to keep in mind when developing your app.
 
 1. Avoid long names of inputs but rather use short IDs. For example, instead of
@@ -35,3 +37,11 @@ The state of a shiny app gets saved by encoding its input values into an URL. To
 2. Use named lists for the `choices` argument in `radioButtons` and `checkboxGroupInput`. Then only the names are displayed to the user allowing for shorter values of the control.
 
 These points are especially relevant for apps with lots of controls.
+
+### Input IDs
+
+Avoid using input IDs which differ by appended numbers, i.e. do not use `value` along with `value2`.
+
+### Action buttons
+
+For action buttons use IDs with a leading dot, e.g. `.myButton`.
