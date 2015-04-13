fea4893dc7895c13eb0721bbd9635ea5f5052ec2
aoles <andrzej.oles@gmail.com>
2015-04-13 17:13:32 +0200

    Fixed indentation and trailing spaces

--- R/shinyURL.R
+++ R/shinyURL.R
@@ -4,7 +4,7 @@
 #' @param session parameter passed from the \code{shinyServer} function
 #' @param inputId id of the text field containing the encoded URL
 #' @import shiny
-#' @export 
+#' @export
 shinyURL = function(session, inputId=".url") {
   .inputValues <<- reactive({
     inputValues = reactiveValuesToList(session$input, all.names=FALSE)
@@ -33,60 +33,59 @@ encodeShinyURL = function(session) {
 
 .encodeURL = function(session, inputId) {
   observe({
-  cat("encode\n")
-  ## all.names = FALSE excludes objects with a leading dot, in this case the ".url" field to avoid self-dependency
-  inputValues = .inputValues()
-  
-  ## NOTE: the default values of checkbox groups are encoded as TRUE/FALSE 
-  ## values of individual elements named as checkboxGroupNameX, where X is the
-  ## element index; the _values_ set by user are stored as a vector under the
-  ## checkboxGroupName
-  
-  ## sort to catch checkboxGroupNames before individual element names  
-  names = sort(names(inputValues))
-  addValues = NULL
-  i = 1L
-  while (i <= length(names) ) {        
-    n = names[i]
-    idx = grep(sprintf("^%s[0-9]+", n), names)    
+    ## all.names = FALSE excludes objects with a leading dot, in this case the ".url" field to avoid self-dependency
+    inputValues = .inputValues()
     
-    if ( length(idx) == 0L ) {
-      ## advance to the next input
-      i = i + 1L
-    } else {
-      ## encountered CheckboxGroup; do not increment i because the corresponding
-      ## element is removed from the list
-      value = inputValues[[n]]
-      names = names[-c(i, idx)]
-      ## this is important to be able to have all checkboxes unchecked
-      if ( is.null(value) ) value = ""
-      values = as.list(value)
-      ## expand CheckboxGroup vectors
-      names(values) = sprintf("%s[%s]", n, seq_along(value))
-      addValues = c(values, addValues)
+    ## NOTE: the default values of checkbox groups are encoded as TRUE/FALSE
+    ## values of individual elements named as checkboxGroupNameX, where X is the
+    ## element index; the _values_ set by user are stored as a vector under the
+    ## checkboxGroupName
+    
+    ## sort to catch checkboxGroupNames before individual element names
+    names = sort(names(inputValues))
+    addValues = NULL
+    i = 1L
+    while (i <= length(names) ) {
+      n = names[i]
+      idx = grep(sprintf("^%s[0-9]+", n), names)
+      
+      if ( length(idx) == 0L ) {
+        ## advance to the next input
+        i = i + 1L
+      } else {
+        ## encountered CheckboxGroup; do not increment i because the corresponding
+        ## element is removed from the list
+        value = inputValues[[n]]
+        names = names[-c(i, idx)]
+        ## this is important to be able to have all checkboxes unchecked
+        if ( is.null(value) ) value = ""
+        values = as.list(value)
+        ## expand CheckboxGroup vectors
+        names(values) = sprintf("%s[%s]", n, seq_along(value))
+        addValues = c(values, addValues)
+      }
     }
-  }
-  inputValues = inputValues[names]
-  inputValues = c(inputValues, addValues)
-  
-  ## encode TRUE/FALSE as T/F
-  inputValues = lapply(inputValues, function(x) {
-    if ( is.logical(x) ) {
-      if (isTRUE(x)) "T" else "F"
-    } else x  
-  })
-  
-  url = paste0(
-    session$clientData$url_protocol, "//",
-    session$clientData$url_hostname,
-    ## add port number if present
-    if( (port=session$clientData$url_port)!="" ) paste0(":", port),
-    session$clientData$url_pathname,        
-    ## encode widget state
-    "?", paste(names(inputValues), inputValues, sep = "=", collapse = "&") 
-  )
-  
-  updateTextInput(session, inputId, value = url)
+    inputValues = inputValues[names]
+    inputValues = c(inputValues, addValues)
+    
+    ## encode TRUE/FALSE as T/F
+    inputValues = lapply(inputValues, function(x) {
+      if ( is.logical(x) ) {
+        if (isTRUE(x)) "T" else "F"
+      } else x
+    })
+    
+    url = paste0(
+      session$clientData$url_protocol, "//",
+      session$clientData$url_hostname,
+      ## add port number if present
+      if( (port=session$clientData$url_port)!="" ) paste0(":", port),
+      session$clientData$url_pathname,
+      ## encode widget state
+      "?", paste(names(inputValues), inputValues, sep = "=", collapse = "&")
+    )
+    
+    updateTextInput(session, inputId, value = url)
   }, priority = -999)
 }
 
@@ -105,23 +104,20 @@ initFromURL = function(session, nestedDependency = FALSE, self, encode, resume =
 
 .initFromURL = function(session, self) {
   observe({
-    cat("init\n")
-    ## suspend if nothing to do 
+    ## suspend if nothing to do
     if ( length(.queryValues) == 0L ) {
-      cat("suspend\n")
       self$suspend()
       return(NULL)
     }
-
+    
     ## iterate through available inputs as long as there are any uninitialized values in .queryValues
-    ## the expression below depends on inputs which is neccassary to restore dynamic UIs  
+    ## the expression below depends on inputs which is neccassary to restore dynamic UIs
     updateValues = intersect(names(.inputValues()), names(.queryValues))
-    cat(updateValues, "\n") 
     idx = match(updateValues, names(.queryValues))
     updateValues = .queryValues[idx]
-
+    
     if ( length(idx) > 0 ) .queryValues <<- .queryValues[-idx]
-   
+    
     ## schedule the update only after all input messages have been sent out (see
     ## the 'flushOutput' function in shiny.R). This is to avoid potential
     ## overwriting by some update events from user code
