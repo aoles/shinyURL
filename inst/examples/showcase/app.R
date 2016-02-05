aeebc923256d23c444bb3e67c4d770c2df60087e
aoles <andrzej.oles@gmail.com>
2016-02-05 19:08:01 +0100

    Extend the showcase app

--- inst/examples/showcase/app.R
+++ inst/examples/showcase/app.R
@@ -6,25 +6,53 @@ shinyApp(
     sidebarLayout(
       sidebarPanel(
         selectInput("var", "Variable:", list(`Eruption time` = "eruptions", `Waiting time` = "waiting")),
-        sliderInput("bins", "Number of bins:", min = 1,  max = 50, value = 30),
-        shinyURL.ui()
+        shinyURL.ui(),
+        div(id = "description", includeMarkdown("description.md"))
       ),
       mainPanel(
         tabsetPanel(id = "tab", 
-                    tabPanel("Plot", plotOutput("plot")), 
+                    tabPanel("Plot",
+                             plotOutput("plot"),
+                             radioButtons(inputId = "bins",
+                                         label = "Number of bins:",
+                                         choices = c(10, 20, 35, 50),
+                                         selected = 20,
+                                         inline = TRUE),
+                             checkboxInput(inputId = "density",
+                                           label = strong("Show density estimate"),
+                                           value = FALSE),
+                             # Display this only if the density is shown
+                             conditionalPanel(condition = "input.density == true",
+                                              sliderInput(inputId = "bandwidth",
+                                                          label = "Bandwidth adjustment:",
+                                                          min = 0.2, max = 2, value = 1, step = 0.2)
+                             )
+                    ), 
                     tabPanel("Summary", verbatimTextOutput("summary"))
         )
       )
     )
   ),
+  
   server = function(input, output) {
     shinyURL.server()
+    
     data <- reactive(faithful[, input$var])
+    
     output$plot <- renderPlot({
       x <- data()
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      bins <- seq(min(x), max(x), length.out = as.integer(input$bins) + 1)
+      hist(x,
+           breaks = bins,
+           probability = TRUE,
+           col = 'darkgray',
+           border = 'white',
+           xlab = paste(input$var, "[minutes]"))
+      if (input$density)
+        lines(density(x, adjust = input$bandwidth), col = "red")
     })
+    
     output$summary <- renderPrint(summary(data()))
+    
   }
 )
---
+++ inst/examples/showcase/description.md
@@ -0,0 +1,5 @@
+This app demonstrates how to use the package [shinyURL](https://github.com/aoles/shinyURL) to save and restore the view state of a Shiny app.
+
+For more details, please see the package [documentation](https://github.com/aoles/shinyURL/blob/master/README.md).
+
+Created by Andrzej Oleś, 2016 &bull; Code on [GitHub](https://github.com/aoles/shinyURL/blob/master/inst/examples/showcase)
